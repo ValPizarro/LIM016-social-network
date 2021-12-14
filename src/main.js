82e53c05e479ac2cdb26c6dabe99a8ba75480415
Valeria Pizarro <valpizarro.ce@outlook.com>
2021-12-13 22:06:23 -0500

    función mostrar secciones

--- src/main.js
+++ src/main.js
@@ -3,3 +3,32 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+// --- declaración de variables ----->
+    // Section:
+let welcomeSection = document.getElementById("welcome");
+let loginSection = document.getElementById("login");
+let registerSection = document.getElementById("register");
+
+    // Botones: 
+const btnjoin = document.getElementById("btnJoin");
+const btnLog = document.getElementById("btnLog");
+const btnSingUp = document.getElementById("btnSignUp");
+
+// -------  funciones mostrar secciones ----->
+
+function join() {
+    btnjoin.addEventListener("click", () => {
+    welcomeSection.classList.add("hidden");
+    loginSection.classList.remove("hidden");
+    registerSection.classList.add("hidden");
+    })
+} join();
+
+function register() {
+    btnSingUp.addEventListener("click", () => {
+    welcomeSection.classList.add("hidden");
+    loginSection.classList.add("hidden");
+    registerSection.classList.remove("hidden");
+  })
+} register();
