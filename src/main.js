f09f8785044fa20e1d9bdbf856f9a90df0ebfd46
DollyPilar <llyatidum@gmail.com>
2021-12-14 20:02:34 -0500

    corrección main.js

--- src/main.js
+++ src/main.js
@@ -5,30 +5,30 @@ import { myFunction } from './lib/index.js';
 myFunction();
 
 // --- declaración de variables ----->
-    // Section:
-let welcomeSection = document.getElementById("welcome");
-let loginSection = document.getElementById("login");
-let registerSection = document.getElementById("register");
+// Section:
+const welcomeSection = document.getElementById('welcome');
+const loginSection = document.getElementById('login');
+const registerSection = document.getElementById('register');
 
-    // Botones: 
-const btnjoin = document.getElementById("btnJoin");
-const btnLog = document.getElementById("btnLog");
-const btnSingUp = document.getElementById("btnSignUp");
+// Botones:
+const btnjoin = document.getElementById('btnJoin');
+// const btnLog = document.getElementById('btnLog');
+const btnSingUp = document.getElementById('btnSignUp');
 
 // -------  funciones mostrar secciones ----->
 
 function join() {
-    btnjoin.addEventListener("click", () => {
-    welcomeSection.classList.add("hidden");
-    loginSection.classList.remove("hidden");
-    registerSection.classList.add("hidden");
-    })
+  btnjoin.addEventListener('click', () => {
+    welcomeSection.classList.add('hidden');
+    loginSection.classList.remove('hidden');
+    registerSection.classList.add('hidden');
+  });
 } join();
 
 function register() {
-    btnSingUp.addEventListener("click", () => {
-    welcomeSection.classList.add("hidden");
-    loginSection.classList.add("hidden");
-    registerSection.classList.remove("hidden");
-  })
+  btnSingUp.addEventListener('click', () => {
+    welcomeSection.classList.add('hidden');
+    loginSection.classList.add('hidden');
+    registerSection.classList.remove('hidden');
+  });
 } register();
