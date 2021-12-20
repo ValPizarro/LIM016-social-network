2ed0756be739cf2760735afd7bf2d6fad8d12191
Valeria Pizarro <valpizarro.ce@outlook.com>
2021-12-19 19:52:32 -0500

    Firebase

---
+++ src/firebase/configuraciones.js
@@ -0,0 +1,26 @@
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup} from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
+// [START auth_signup_password_modular]
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCYMriyYLnj7mjwQ990OLhGaxulpUI6ONE',
+  authDomain: 'queerplace-lim16.firebaseapp.com',
+  projectId: 'queerplace-lim16',
+  storageBucket: 'queerplace-lim16.appspot.com',
+  messagingSenderId: '1092619679372',
+  appId: '1:1092619679372:web:0b0ca2cc48f7cbf0256da9',
+  measurementId: 'G-KEF6M5WVLL',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider(app);
+
+
+export {
+  app, auth, provider, createUserWithEmailAndPassword,
+  signInWithEmailAndPassword, signOut, signInWithPopup,
+};
--- src/main.js
+++ src/main.js
@@ -12,8 +12,8 @@ const registerSection = document.getElementById('register');
 
 // Botones:
 const btnjoin = document.getElementById('btnJoin');
-// const btnLog = document.getElementById('btnLog');
-const btnSingUp = document.getElementById('btnSignUp');
+// const btnLog = document.getElementById('btnSignUp');
+const btnSingUp = document.getElementById('btnLog');
 
 /**
  * funciones mostrar sección log in */
@@ -28,9 +28,39 @@ function join() {
 /**
  * funciones mostrar sección registro */
 function register() {
-  btnSingUp.addEventListener('click', () => {
+  btnSingUp.addEventListener('click', (e) => {
+    e.preventDefault();
+
     welcomeSection.classList.add('hidden');
     loginSection.classList.add('hidden');
     registerSection.classList.remove('hidden');
   });
 } register();
+
+/**
+ * funciones firebase*/
+const logInForm = document.querySelector('#formLogIn');
+
+logInForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const email = document.querySelector('#logEmail').value;
+  const password = document.querySelector('#logPassword').value;
+
+  console.log(email, password);
+});
+
+
+const registerForm = document.querySelector('#formResgister');
+registerForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const email = document.querySelector('#registerEmail').value;
+  const password = document.querySelector('#registerPassword').value;
+
+  auth
+      .createUserWithEmailAndPassword(email, password);
+  // .then(userCredential => {
+  //   console.log('sing up');
+  // });
+});
