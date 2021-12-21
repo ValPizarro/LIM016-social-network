6e436f02475657c862f2b7b213ea4735a5b0a1e5
Valeria Pizarro <valpizarro.ce@outlook.com>
2021-12-20 19:23:29 -0500

    Firebase: Sign In with email

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -4,20 +4,22 @@ import {
 } from './configuraciones.js';
 
 export const createUser = (email, password) => {
-  // return createUserWithEmailAndPassword(auth, email, password);
   return createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
-
         // console.log('usario creado', {userCredential});
         console.log('usario creado', {user});
       });
 };
 
-
 export const signIn = (email, password) => {
-  return signInWithEmailAndPassword(auth, email, password);
+  return signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log('Inició sesión', {user});
+      });
 };
+
 export const signOutUser = () => {
   return signOut(auth);
 };
