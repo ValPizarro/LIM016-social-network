7996c49edbdcde295f8b02e655023c7d91715494
ValPizarro <valpizarro.ce@outlook.com>
2022-02-09 17:39:36 -0500

    Mock firebase

--- src/firebase/__mocks__/configuraciones.js
+++ src/firebase/__mocks__/configuraciones.js
@@ -3,8 +3,10 @@ auth.currentUser = {
   uid: 'fake-user-id',
 };
 
-export const setDoc = jest.fn((document, values) => Promise.resolve({}));
+export const setDoc = jest.fn((document, values) => Promise.resolve(values));
 
 export const doc = jest.fn((db, collection, docID) => Promise.resolve({}));
 
 export const db = jest.fn({});
+
+export const updateDoc = jest.fn({});
