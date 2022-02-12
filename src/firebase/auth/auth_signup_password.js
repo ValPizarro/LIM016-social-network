311efc63a6c4f36b176e97c26fede62393cddbf8
Valeria Pizarro <valpizarro.ce@outlook.com>
2022-02-12 13:15:23 -0500

    Verifición del usuario

--- src/firebase/auth/auth_signup_password.js
+++ src/firebase/auth/auth_signup_password.js
@@ -1,29 +1,31 @@
-import {onAuth} from './auth_state_listener.js';
+// import {onAuth} from './auth_state_listener.js';
 import {
   auth, createUserWithEmailAndPassword,
   sendEmailVerification,
 } from '../configuraciones.js';
 import {showError} from '../../view/signin.js';
+import {backSignIn} from '../../view/signup.js';
+
 
 export const createUser = (email, password) => {
   return createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
         console.log(user);
-        userVerification();
-        onAuth();
+        userVerification()
+            .then(() => {
+              alert('Correo de verificación enviado');
+              backSignIn();
+            });
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        console.log('Error', errorMessage);
+        console.error(errorMessage);
         showError(errorCode);
       });
 };
 
 export const userVerification = () => {
-  return sendEmailVerification(auth.currentUser)
-      .them(() => {
-        backSignIn();
-      });
+  return sendEmailVerification(auth.currentUser);
 };
