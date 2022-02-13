7064a576d0bd8ce6e25197514b5c0aff6c29ffdd
ElianeRiveraCastillo22 <elianercast@gmail.com>
2022-02-13 10:19:33 -0500

    implentacion del css

--- src/view/nav.js
+++ src/view/nav.js
@@ -47,18 +47,15 @@ const nav = () => {
   //       .addEventListener('click', registerUser);
   // } else {
   const showNav = `
+
+
     <nav class="tab-nav-container">
+      <div class="tabTitle">
+        <p>Queer Place</p>
+      </div>
       <div class="tab">
         <i id="home" class="fas fa-home fasNav"></i>
-      </div>
-      <div class="tab">
-        <div class="boxSearch">
-          <div name="search">
-            <input  type="text" class="inputSearch" name="txt" >
-          </div>
-          <i id="search" class="fas fa-search fasNav"></i>
-        </div>
-      </div>
+      </div>  
       <div class="tab">
         <i id="profile" class="fas fa-user fasNav"></i>
       </div>
@@ -75,7 +72,7 @@ const nav = () => {
 
   navElemt.querySelector('#profile').addEventListener('click', showProfile);
 
-  navElemt.querySelector('#search').addEventListener('click', showSearch);
+ // navElemt.querySelector('#search').addEventListener('click', showSearch);
 
   navElemt.querySelector('#signOut').addEventListener('click', showSignOut);
   // }
